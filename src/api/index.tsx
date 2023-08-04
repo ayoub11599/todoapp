8ca2f7531040582d9ce11f119118e8fcefb0c778
ayoub11599 <141245286+ayoub11599@users.noreply.github.com>
2023-08-04 18:05:15 +0100

    Delete src/api directory

--- src/api/index.tsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "http://localhost:3000",
-    headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-});
